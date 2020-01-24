1825836cbd4b8b8020a3f648cafc564165cd3bf8
BuildTools <unconfigured@null.spigotmc.org>
2020-01-24 23:12:44 +0100

    Fixed void generator for 1.15+

--- src/me/Straiker123/Utils/voidGenerator.java
+++ src/me/Straiker123/Utils/voidGenerator.java
@@ -14,12 +14,9 @@ public class voidGenerator extends ChunkGenerator {
 	        ChunkGenerator.ChunkData data = this.createChunkData(world);
 	        for (int x = 0; x < 16; ++x) {
 	            for (int z = 0; z < 16; ++z) {
-	            	if(TheAPI.getServerVersion().contains("glowstone")||TheAPI.getServerVersion().contains("1_14"))the_void="VOID";
-	            	if(TheAPI.getServerVersion().contains("1_13")||TheAPI.getServerVersion().contains("1_14"))the_void="THE_VOID";
-	            	if(TheAPI.getServerVersion().contains("1_12")||TheAPI.getServerVersion().contains("1_11")
-	            			||TheAPI.getServerVersion().contains("1_10")||TheAPI.getServerVersion().contains("1_9"))the_void="VOID";
+	            	if(!TheAPI.isNewVersion())the_void="VOID";
+	            	else the_void="THE_VOID";
 	                biome.setBiome(x, z, Biome.valueOf(the_void));
-	                
 	            }
 	        }
 	        return data;
