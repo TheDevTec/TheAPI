9e243507117d5326e4071c2492e79b0a400f8fe4
BuildTools <unconfigured@null.spigotmc.org>
2020-04-26 22:56:18 +0200

    Added new methods

--- src/me/Straiker123/ConfigAPI.java
+++ src/me/Straiker123/ConfigAPI.java
@@ -5,10 +5,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
+import org.bukkit.Color;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.inventory.ItemStack;
 
 import com.google.common.io.Files;
 
@@ -17,18 +22,32 @@ import me.Straiker123.Utils.Error;
 public class ConfigAPI {
 	private String name,h, loc,end="yml";
 	private FileConfiguration a;
-	private File f;
+	private File f,folder;
 	private Map<String, Object> c=new HashMap<String, Object>();
 	
 	public ConfigAPI(String localization, String configName) {
 	name=configName;	
 	loc=localization;
 	}
-	/**
-	 * @return boolean if exist string in config (Can be used before create a config)
-	 */
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getFolderName() {
+		return loc;
+	}
+	
+	public File getFolder() {
+		if(folder==null)
+		folder=new File(loc);
+		if(!folder.exists())
+		folder.mkdir();
+		return folder;
+	}
+	
 	public boolean existPath(String string) {
-		return existFile() && YamlConfiguration.loadConfiguration(getFile()).getString(string) != null;
+		return getString(string) != null;
 	}
 	
 	public void addDefault(String path, Object value) {
@@ -37,8 +56,12 @@ public class ConfigAPI {
 		else
 			c.replace(path, value);
 	}
-	
+
 	public void addDefaults(Map<String, Object> defaults){
+		c=defaults;
+	}
+
+	public void addDefaults(HashMap<String, Object> defaults){
 		c=defaults;
 	}
 
@@ -48,10 +71,7 @@ public class ConfigAPI {
 	
 	public File getFile() {
 		if(f==null) {
-			File d = new File("plugins/"+loc);
-			if(!d.mkdir())
-			d.mkdirs();
-			File ff = new File("plugins/"+loc+"/"+name+"."+end);
+			File ff = new File(getFolder(),name+"."+end);
 			try {
 			if(ff.exists()) { //file exists
 				f=ff;
@@ -70,7 +90,7 @@ public class ConfigAPI {
 		f= ff;
 		}
 		}catch(Exception er) {
-			ff = new File("plugins/"+loc+"/"+name+"-fixed."+end);
+			ff = new File(getFolder(),name+"-fixed."+end);
 			try {
 				ff.createNewFile();
 				f= ff;
@@ -99,24 +119,27 @@ public class ConfigAPI {
 	}
 
 	public boolean removePath(String path) {
-		if(a!=null) {
-			if(a.getString(path) != null) {
-				a.set(path, null);
+			if(getString(path) != null) {
+				set(path, null);
 				return true;
 			}
-		}
 		return false;
 	}
 	public boolean createPath(String path) {
-		if(a!=null) {
-			if(a.getString(path) == null) {
-				a.createSection(path);
+			if(getString(path) == null) {
+				createSection(path);
 				return true;
 			}
-		}
 		return false;
 	}
 	
+	public void createSection(String path) {
+		if(!existFile()) return;
+		if(a==null)
+		YamlConfiguration.loadConfiguration(getFile()).createSection(path);
+		a.createSection(path);
+	}
+
 	private boolean check() {
 		if(a==null) {
 			TheAPI.getConsole().sendMessage(TheAPI.colorize("&bTheAPI&7: &cFileConfiguration is null"));
@@ -172,6 +195,146 @@ public class ConfigAPI {
 				Error.sendRequest("&bTheAPI&7: &cError when reloading "+name+"."+end+" config");
 			return false;
 		}
+	}
+	
+	public int getInt(String path) {
+		if(!existFile()) return 0;
+		if(a==null)
+		return YamlConfiguration.loadConfiguration(getFile()).getInt(path);
+		return a.getInt(path);
+	}
+	
+	public double getDouble(String path) {
+		if(!existFile()) return 0;
+		if(a==null)
+		return YamlConfiguration.loadConfiguration(getFile()).getDouble(path);
+		return a.getDouble(path);
+	}
+	
+	public String getString(String path) {
+		if(!existFile()) return null;
+		if(a==null)
+		return YamlConfiguration.loadConfiguration(getFile()).getString(path);
+		return a.getString(path);
+	}
+	
+	public long getLong(String path) {
+		if(!existFile()) return 0;
+		if(a==null)
+		return YamlConfiguration.loadConfiguration(getFile()).getLong(path);
+		return a.getLong(path);
+	}
+	
+	public boolean getBoolean(String path) {
+		if(!existFile()) return false;
+		if(a==null)
+		return YamlConfiguration.loadConfiguration(getFile()).getBoolean(path);
+		return a.getBoolean(path);
+	}
+	
+	public ConfigurationSection getConfigurationSection(String path) {
+		if(!existFile()) return null;
+		if(a==null)
+		return YamlConfiguration.loadConfiguration(getFile()).getConfigurationSection(path);
+		return a.getConfigurationSection(path);
+	}
+	
+	public Set<String> getKeys(boolean keys) {
+		if(!existFile()) return null;
+		if(a==null)
+		return YamlConfiguration.loadConfiguration(getFile()).getKeys(keys);
+		return a.getKeys(keys);
+	}
+	
+	public Set<String> getConfigurationSection(String path, boolean keys) {
+		if(!existFile()) return null;
+		if(a==null)
+		return YamlConfiguration.loadConfiguration(getFile()).getConfigurationSection(path).getKeys(keys);
+		return a.getConfigurationSection(path).getKeys(keys);
+	}
+	
+	public void set(String path, Object value) {
+		if(!existFile()) return;
+		if(a==null)
+		YamlConfiguration.loadConfiguration(getFile()).set(path,value);
+		a.set(path,value);
+	}
+	
+	public boolean isString(String path) {
+		if(!existFile()) return false;
+		if(a==null)
+		return YamlConfiguration.loadConfiguration(getFile()).isString(path);
+		return a.isString(path);
+	}
+	
+	public boolean isLong(String path) {
+		if(!existFile()) return false;
+		if(a==null)
+		return YamlConfiguration.loadConfiguration(getFile()).isLong(path);
+		return a.isLong(path);
+	}
+	
+	public boolean isDouble(String path) {
+		if(!existFile()) return false;
+		if(a==null)
+		return YamlConfiguration.loadConfiguration(getFile()).isDouble(path);
+		return a.isDouble(path);
+	}
+	
+	public boolean isInt(String path) {
+		if(!existFile()) return false;
+		if(a==null)
+		return YamlConfiguration.loadConfiguration(getFile()).isBoolean(path);
+		return a.isInt(path);
+	}
+
+	public boolean isBoolean(String path) {
+		if(!existFile()) return false;
+		if(a==null)
+		return YamlConfiguration.loadConfiguration(getFile()).isBoolean(path);
+		return a.isBoolean(path);
+	}
+
+	public boolean isConfigurationSection(String path) {
+		if(!existFile()) return false;
+		if(a==null)
+		return YamlConfiguration.loadConfiguration(getFile()).isConfigurationSection(path);
+		return a.isConfigurationSection(path);
+	}
+
+	public Object get(String path) {
+		if(!existFile()) return null;
+		if(a==null)
+		return YamlConfiguration.loadConfiguration(getFile()).get(path);
+		return a.get(path);
+	}
+
+	public List<String> getStringList(String path) {
+		if(!existFile()) return null;
+		if(a==null)
+		return YamlConfiguration.loadConfiguration(getFile()).getStringList(path);
+		return a.getStringList(path);
+	}
+
+	public List<?> getList(String path) {
+		if(!existFile()) return null;
+		if(a==null)
+		return YamlConfiguration.loadConfiguration(getFile()).getList(path);
+		return a.getList(path);
+	}
+
+	public Color getColor(String path) {
+		if(!existFile()) return null;
+		if(a==null)
+		return YamlConfiguration.loadConfiguration(getFile()).getColor(path);
+		return a.getColor(path);
+	}
+
+	public ItemStack getItemStack(String path) {
+		if(!existFile()) return null;
+		if(a==null)
+		return YamlConfiguration.loadConfiguration(getFile()).getItemStack(path);
+		return a.getItemStack(path);
 	}
 
 	public boolean create() {
