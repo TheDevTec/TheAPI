cc5c568c8024c90aac2c2810b0a2ac8b0785d47b
BuildTools <unconfigured@null.spigotmc.org>
2020-06-20 16:06:54 +0200

    Now registering Placeholders to TheAPI too

--- src/me/DevTec/Placeholders/PlaceholderAPIUtils.java
+++ src/me/DevTec/Placeholders/PlaceholderAPIUtils.java
@@ -4,37 +4,54 @@ import java.util.List;
 
 import org.bukkit.OfflinePlayer;
 
+import com.google.common.collect.Lists;
+
 import me.DevTec.TheAPI;
 import me.DevTec.NMS.Reflections;
 
 public class PlaceholderAPIUtils {
-
+	private static List<PlaceholderRegister> reg = Lists.newArrayList();
 	public boolean isEnabledPlaceholderAPI() {
 		return TheAPI.getPluginsManagerAPI().isEnabledPlugin("PlaceholderAPI");
 	}
 
 	public String setPlaceholders(OfflinePlayer player, String where) {
+		String edited = where;
+		for(PlaceholderRegister r : reg)edited=r.onRequest(player, edited);
 		if (isEnabledPlaceholderAPI())
-			where = (String)Reflections.invoke(null, Reflections.getMethod(Reflections.getClass("me.clip.placeholderapi.PlaceholderAPI"),
-					"setPlaceholders",OfflinePlayer.class,String.class),player, where);
-		return where;
+			edited = (String)Reflections.invoke(null, Reflections.getMethod(Reflections.getClass("me.clip.placeholderapi.PlaceholderAPI"),
+					"setPlaceholders",OfflinePlayer.class,String.class),player, edited);
+		return edited;
 	}
 
 	@SuppressWarnings("unchecked")
 	public List<String> setPlaceholders(OfflinePlayer player, List<String> where) {
+		List<String> edited = Lists.newArrayList();
+		for(String s : where) {
+			for(PlaceholderRegister r : reg)s=r.onRequest(player, s);
+			edited.add(s);
+		}
 		if (isEnabledPlaceholderAPI())
-			where = (List<String>)Reflections.invoke(null, Reflections.getMethod(Reflections.getClass("me.clip.placeholderapi.PlaceholderAPI"),
+			edited = (List<String>)Reflections.invoke(null, Reflections.getMethod(Reflections.getClass("me.clip.placeholderapi.PlaceholderAPI"),
 					"setPlaceholders",OfflinePlayer.class,List.class),player, where);
-		return where;
+		return edited;
+	}
+	
+	public boolean isRegistredPlaceholder(PlaceholderRegister e) {
+		return reg.contains(e);
 	}
 	
 	public void registerPlaceholder(PlaceholderRegister e) {
+		if(isRegistredPlaceholder(e))return;
+		reg.add(e);
 		if (isEnabledPlaceholderAPI())
 			Reflections.invoke(null, Reflections.getMethod(Reflections.getClass("me.clip.placeholderapi.PlaceholderAPI"),
 					"registerExpansion",Reflections.getClass("me.clip.placeholderapi.expansion.PlaceholderExpansion")),e);
 	}
 	
 	public void unregisterPlaceholder(PlaceholderRegister e) {
+		if(!isRegistredPlaceholder(e))return;
+		reg.remove(e);
 		if (isEnabledPlaceholderAPI())
 			Reflections.invoke(null, Reflections.getMethod(Reflections.getClass("me.clip.placeholderapi.PlaceholderAPI"),
 					"unregisterExpansion",Reflections.getClass("me.clip.placeholderapi.expansion.PlaceholderExpansion")),e);
