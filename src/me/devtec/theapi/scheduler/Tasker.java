cbeafa21a3b96f6a4f28107d1670e53de5c2c697
StraikerinaCZ <Straiker123@email.cz>
2021-03-16 18:42:56 +0100

    Improved Tasker methods

--- src/me/devtec/theapi/scheduler/Tasker.java
+++ src/me/devtec/theapi/scheduler/Tasker.java
@@ -2,80 +2,90 @@ package me.devtec.theapi.scheduler;
 
 public abstract class Tasker implements Runnable {
 	private boolean cancel;
-	private int task;
+	private int task = -1;
 
-	public synchronized boolean isCancelled() {
+	public final synchronized boolean isCancelled() {
 		return cancel;
 	}
 
-	public synchronized void cancel() {
+	public final synchronized void cancel() {
+		if(task==-1)return;
 		cancel = true;
 		Scheduler.cancelTask(task);
 	}
 
-	public synchronized int getId() {
+	public final synchronized int getId() {
 		return task;
 	}
 
-	public int runTask() {
+	//ASYNCHRONOUS
+	
+	public final int runTask() {
+		cancel=false;
 		return task = Scheduler.run(this);
 	}
 
-	public int runRepeating(long delay, long period) {
+	public final int runRepeating(long delay, long period) {
+		cancel=false;
 		if (task == 0)
 			return task = Scheduler.repeating(delay, period, this);
 		return task;
 	}
 
-	public int runTimer(long delay, long period, long times) {
-		return runRepeatingTimes(delay, period, times);
+	public final int runTimer(long delay, long period, long times) {
+		return runRepeatingTimes(delay, period, times, null);
 	}
 
-	public int runRepeatingTimes(long delay, long period, long times) {
-		if (task == 0)
-			return task = Scheduler.repeatingTimes(delay, period, times, this);
-		return task;
+	public final int runRepeatingTimes(long delay, long period, long times) {
+		return runRepeatingTimes(delay, period, times, null);
 	}
 
-	public int runRepeatingTimes(long delay, long period, long times, Runnable onFinish) {
+	public final int runRepeatingTimes(long delay, long period, long times, Runnable onFinish) {
+		cancel=false;
+		cancel=false;
 		if (task == 0)
 			return task = Scheduler.repeatingTimes(delay, period, times, this, onFinish);
 		return task;
 	}
 
-	public int runLater(long delay) {
+	public final int runLater(long delay) {
+		cancel=false;
 		if (task == 0)
 			return task = Scheduler.later(delay, this);
 		return task;
 	}
+	
+	//SYNCHRONOUS WITH SERVER
 
-	public int runTaskSync() {
+	public final int runTaskSync() {
+		cancel=false;
 		return task = Scheduler.runSync(this);
 	}
 
-	public int runRepeatingSync(long delay, long period) {
+	public final int runRepeatingSync(long delay, long period) {
+		cancel=false;
 		if (task == 0)
 			return task = Scheduler.repeatingSync(delay, period, this);
 		return task;
 	}
 
-	public int runTimerSync(long delay, long period, long times) {
-		return runRepeatingTimesSync(delay, period, times);
+	public final int runTimerSync(long delay, long period, long times) {
+		return runRepeatingTimesSync(delay, period, times, null);
 	}
 
-	public int runRepeatingTimesSync(long delay, long period, long times) {
-		if (task == 0)
-			return task = Scheduler.repeatingTimesSync(delay, period, times, this);
-		return task;
+	public final int runRepeatingTimesSync(long delay, long period, long times) {
+		return runRepeatingTimesSync(delay, period, times, null);
 	}
 
-	public int runRepeatingTimesSync(long delay, long period, long times, Runnable onFinish) {
+	public final int runRepeatingTimesSync(long delay, long period, long times, Runnable onFinish) {
+		cancel=false;
 		if (task == 0)
 			return task = Scheduler.repeatingTimesSync(delay, period, times, this, onFinish);
 		return task;
 	}
 
-	public int runLaterSync(long delay) {
+	public final int runLaterSync(long delay) {
+		cancel=false;
 		if (task == 0)
 			return task = Scheduler.laterSync(delay, this);
 		return task;
