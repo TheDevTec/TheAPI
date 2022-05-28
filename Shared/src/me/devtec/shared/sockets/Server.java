a8cac5e5effd7d006b3c9c64765fecdcd5b9b56f
StraikerinaCZ <Straiker123@email.cz>
2022-05-28 02:35:40 +0200

    Duplicate code

--- Shared/src/me/devtec/shared/sockets/Server.java
+++ Shared/src/me/devtec/shared/sockets/Server.java
@@ -23,83 +23,15 @@ public class Server {
 	private boolean closed;
 
 	public Server(String password, int port) {
-		this.pas = password;
+		pas = password;
 		try {
-			this.server = new ServerSocket(port);
-			this.server.setSoTimeout(200);
-			new Thread(() -> {
-				while (!Server.this.closed) {
-					Socket receive;
-					try {
-						receive = Server.this.server.accept();
-					} catch (Exception e2) {
-						try {
-							Thread.sleep(7000);
-						} catch (Exception e11) {
-						}
-						continue;
-					}
-					BufferedReader reader;
-					PrintWriter writer;
-					try {
-						reader = new BufferedReader(new InputStreamReader(receive.getInputStream()));
-						writer = new PrintWriter(receive.getOutputStream(), true);
-					} catch (Exception e3) {
-						try {
-							receive.close();
-						} catch (Exception e12) {
-						}
-						try {
-							Thread.sleep(7000);
-						} catch (Exception e13) {
-						}
-						continue;
-					}
-					ClientHandler handler = new ClientHandler(Server.this, receive, reader, writer);
-					Server.this.sockets.put(receive, new ServerClient(handler));
-					handler.start();
-				}
-			}).start();
+			startServer(port);
 		} catch (Exception e) {
 			new Tasker() {
 				@Override
 				public void run() {
 					try {
-						Server.this.server = new ServerSocket(port);
-						Server.this.server.setSoTimeout(200);
-						new Thread(() -> {
-							while (!Server.this.closed) {
-								Socket receive;
-								try {
-									receive = Server.this.server.accept();
-								} catch (Exception e2) {
-									try {
-										Thread.sleep(7000);
-									} catch (Exception e11) {
-									}
-									continue;
-								}
-								BufferedReader reader;
-								PrintWriter writer;
-								try {
-									reader = new BufferedReader(new InputStreamReader(receive.getInputStream()));
-									writer = new PrintWriter(receive.getOutputStream(), true);
-								} catch (Exception e3) {
-									try {
-										receive.close();
-									} catch (Exception e12) {
-									}
-									try {
-										Thread.sleep(7000);
-									} catch (Exception e13) {
-									}
-									continue;
-								}
-								ClientHandler handler = new ClientHandler(Server.this, receive, reader, writer);
-								Server.this.sockets.put(receive, new ServerClient(handler));
-								handler.start();
-							}
-						}).start();
+						startServer(port);
 					} catch (Exception e) {
 						e.printStackTrace();
 					}
@@ -108,60 +40,98 @@ public class Server {
 		}
 	}
 
+	private final void startServer(int port) throws Exception {
+		server = new ServerSocket(port);
+		server.setSoTimeout(200);
+		new Thread(() -> {
+			while (!Server.this.closed) {
+				Socket receive;
+				try {
+					receive = Server.this.server.accept();
+				} catch (Exception e2) {
+					try {
+						Thread.sleep(7000);
+					} catch (Exception e11) {
+					}
+					continue;
+				}
+				BufferedReader reader;
+				PrintWriter writer;
+				try {
+					reader = new BufferedReader(new InputStreamReader(receive.getInputStream()));
+					writer = new PrintWriter(receive.getOutputStream(), true);
+				} catch (Exception e3) {
+					try {
+						receive.close();
+					} catch (Exception e12) {
+					}
+					try {
+						Thread.sleep(7000);
+					} catch (Exception e13) {
+					}
+					continue;
+				}
+				ClientHandler handler = new ClientHandler(Server.this, receive, reader, writer);
+				Server.this.sockets.put(receive, new ServerClient(handler));
+				handler.start();
+			}
+		}).start();
+	}
+
 	public void writeAll(String path, Object object) {
-		for (ServerClient s : this.sockets.values())
+		for (ServerClient s : sockets.values())
 			s.write(path, object);
 	}
 
 	public void write(String client, String path, Object object) {
-		for (ServerClient s : this.sockets.values())
+		for (ServerClient s : sockets.values())
 			if (s.getName().equals(client))
 				s.write(path, object);
 	}
 
 	public void sendAll() {
-		for (ServerClient s : this.sockets.values())
+		for (ServerClient s : sockets.values())
 			s.send();
 	}
 
 	public void send(String client) {
-		for (ServerClient s : this.sockets.values())
+		for (ServerClient s : sockets.values())
 			if (s.getName().equals(client))
 				s.send();
 	}
 
 	protected void read(ServerClient client, final Config data) {
 		EventManager.call(new ClientReceiveMessageEvent(client, data));
-		this.readers.forEach(a -> {
+		readers.forEach(a -> {
 			a.read(client, data);
 		});
 	}
 
 	public void register(Reader reader) {
-		this.readers.add(reader);
+		readers.add(reader);
 	}
 
 	public void unregister(Reader reader) {
-		this.readers.remove(reader);
+		readers.remove(reader);
 	}
 
 	public boolean isClosed() {
-		return this.closed;
+		return closed;
 	}
 
 	public void exit() {
-		this.closed = true;
+		closed = true;
 		try {
-			this.sockets.values().forEach(s -> s.exit());
-			this.sockets.clear();
+			sockets.values().forEach(ServerClient::exit);
+			sockets.clear();
 		} catch (Exception e) {
 		}
 		try {
-			this.server.getChannel().close();
+			server.getChannel().close();
 		} catch (Exception e) {
 		}
 		try {
-			this.server.close();
+			server.close();
 		} catch (Exception e) {
 		}
 	}
