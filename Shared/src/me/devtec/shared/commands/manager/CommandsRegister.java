ab6a21707fc2d0004176962d1256ec9ed0a7c02f
StraikerinaCZ <Straiker123@email.cz>
2022-07-22 18:31:25 +0200

    Changed repo to https://github.com/TheDevTec/TheAPI-Shared

--- Shared/src/me/devtec/shared/commands/manager/CommandsRegister.java
+++
@@ -1,9 +0,0 @@
-package me.devtec.shared.commands.manager;
-
-import me.devtec.shared.commands.holder.CommandHolder;
-
-public interface CommandsRegister {
-
-	public void register(CommandHolder<?> commandHolder, String command, String[] aliases);
-
-}
--- Shared/src/me/devtec/shared/commands/manager/PermissionChecker.java
+++
@@ -1,5 +0,0 @@
-package me.devtec.shared.commands.manager;
-
-public interface PermissionChecker<S> {
-	public boolean has(S sender, String perm, boolean isTablist);
-}
