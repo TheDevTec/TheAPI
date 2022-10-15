37c56f9d39e529b2bf375c2d0514530a52c3b25c
StraikerinaCZ <43417249+StraikerinaCZ@users.noreply.github.com>
2022-10-15 16:07:38 +0200

    Update TestGUI.java

--- Demo/src/me/straikerinacz/theapi/demo/gui/TestGUI.java
+++ Demo/src/me/straikerinacz/theapi/demo/gui/TestGUI.java
@@ -8,20 +8,11 @@ import me.devtec.theapi.bukkit.gui.GUI;
 import me.devtec.theapi.bukkit.gui.ItemGUI;
 
 public class TestGUI {
-	public static void init() {// :'D tak piš hustý, použij setItem nic ti to nezabrazuje či?
+	public static void init() {
 		GUI gui = new GUI("test" , 54);
 		ItemGUI emptyPane = new EmptyItemGUI(ItemMaker.of(Material.BLACK_STAINED_GLASS_PANE).displayName("§7").build());
 		for(int i = 0; i < 10; ++i)
 			gui.setItem(i, emptyPane);
-
-
-		/*
-		 * 9  xxxxxxxxx
-		 * 18 x--------
-		 * 27 ---------
-		 * 36 ---------
-		 * 45 ---------
-		 * 54 ---------
-		 */
+		//gui.open(player);
 	}
 }
