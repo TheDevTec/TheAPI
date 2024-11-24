d71ad042f3026d1846edb7b6afcf16893bda2bc7
Straikerinos <straiker123@email.cz>
2024-11-24 15:02:15 +0100

    Attempt for fix #4

---
+++ bin/build.bat
@@ -0,0 +1,53 @@
+@echo off
+call mvn clean compile verify
+cd NmsProvider - 1.7.R4
+call mvn clean package clean
+cd ..
+cd NmsProvider - 1.8.R3
+call mvn clean package clean
+cd ..
+cd NmsProvider - 1.12.R1
+call mvn clean package clean
+cd ..
+cd NmsProvider - 1.14.R1
+call mvn clean package clean
+cd ..
+cd NmsProvider - 1.16.R3
+call mvn clean package clean
+cd ..
+cd NmsProvider - 1.17.R1
+call mvn clean package clean
+cd ..
+cd NmsProvider - 1.18.R1
+call mvn clean package clean
+cd ..
+cd NmsProvider - 1.18.R2
+call mvn clean package clean
+cd ..
+cd NmsProvider - 1.19.R1
+call mvn clean package clean
+cd ..
+cd NmsProvider - 1.19.R2
+call mvn clean package clean
+cd ..
+cd NmsProvider - 1.19.R3
+call mvn clean package clean
+cd ..
+cd NmsProvider - 1.20.R1
+call mvn clean package clean
+cd ..
+cd NmsProvider - 1.20.R2
+call mvn clean package clean
+cd ..
+cd NmsProvider - 1.20.R3
+call mvn clean package clean
+cd ..
+cd NmsProvider - 1.20.R4
+call mvn clean package clean
+cd ..
+cd NmsProvider - 1.21.R1
+call mvn clean package clean
+cd ..
+cd NmsProvider - 1.20.6
+call mvn clean package clean
+pause
