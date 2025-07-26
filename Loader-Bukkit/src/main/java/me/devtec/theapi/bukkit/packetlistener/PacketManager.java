7a4d53c36d8020d456be33f3b29862cc35c5f629
Straikerinos <straiker123@email.cz>
2025-07-27 00:45:29 +0200

    Fixed errors when disabling plugin

--- Loader-Bukkit/src/main/java/me/devtec/theapi/bukkit/packetlistener/PacketManager.java
+++ Loader-Bukkit/src/main/java/me/devtec/theapi/bukkit/packetlistener/PacketManager.java
@@ -6,79 +6,78 @@ import java.util.List;
 import java.util.Map;
 
 public class PacketManager {
-    private static final Priority[] PRIORITIES = {Priority.LOWEST, Priority.LOW, Priority.NORMAL, Priority.HIGH, Priority.HIGHEST, Priority.MONITOR};
+	private static final Priority[] PRIORITIES = { Priority.LOWEST, Priority.LOW, Priority.NORMAL, Priority.HIGH,
+			Priority.HIGHEST, Priority.MONITOR };
 
-    private static final Map<Priority, List<PacketListener>> listeners = new HashMap<>();
+	private static final Map<Priority, List<PacketListener>> listeners = new HashMap<>();
 
-    static {
-        for (Priority priority : PRIORITIES) {
+	static {
+		for (Priority priority : PRIORITIES)
 			PacketManager.listeners.put(priority, new ArrayList<>());
-		}
-    }
+	}
 
-    public static Object call(String player, final Object packet, final Object channel, PacketType type) {
-        if (packet == null || channel == null) {
+	public static Object call(String player, final Object packet, final Object channel, PacketType type) {
+		if (packet == null || channel == null)
 			return packet;
-		}
 
-        PacketContainer pContainer = new PacketContainer(packet);
-        ChannelContainer cContainer = new ChannelContainer(channel);
+		PacketContainer pContainer = new PacketContainer(packet);
+		ChannelContainer cContainer = new ChannelContainer(channel);
 
-        if (type == PacketType.PLAY_OUT) {
+		if (type == PacketType.PLAY_OUT)
 			for (Priority o : PRIORITIES) {
-				for (PacketListener w : PacketManager.listeners.get(o)) {
-					w.playOut(player, pContainer, cContainer);
+				List<PacketListener> listeners = PacketManager.listeners.get(o);
+				if (listeners.size() > 0) {
+					int pos = 0;
+					for (PacketListener w = listeners.get(pos); pos < listeners.size(); ++pos)
+						w.playOut(player, pContainer, cContainer);
 				}
 			}
-		} else {
+		else
 			for (Priority o : PRIORITIES) {
-				for (PacketListener w : PacketManager.listeners.get(o)) {
-					w.playIn(player, pContainer, cContainer);
+				List<PacketListener> listeners = PacketManager.listeners.get(o);
+				if (listeners.size() > 0) {
+					int pos = 0;
+					for (PacketListener w = listeners.get(pos); pos < listeners.size(); ++pos)
+						w.playIn(player, pContainer, cContainer);
 				}
 			}
-		}
-        return pContainer.isCancelled() ? null : pContainer.getPacket();
-    }
+		return pContainer.isCancelled() ? null : pContainer.getPacket();
+	}
 
-    public static void register(PacketListener listener) {
-        PacketManager.notify(listener, null, listener.getPriority());
-    }
+	public static void register(PacketListener listener) {
+		PacketManager.notify(listener, null, listener.getPriority());
+	}
 
-    public static void unregister(PacketListener listener) {
-        PacketManager.listeners.get(listener.getPriority()).remove(listener);
-    }
+	public static void unregister(PacketListener listener) {
+		PacketManager.listeners.get(listener.getPriority()).remove(listener);
+	}
 
-    public static void setPriority(PacketListener listener, Priority priority) {
-        PacketManager.notify(listener, listener.getPriority(), priority);
-        listener.priority = priority;
-    }
+	public static void setPriority(PacketListener listener, Priority priority) {
+		PacketManager.notify(listener, listener.getPriority(), priority);
+		listener.priority = priority;
+	}
 
-    public static Priority getPriority(PacketListener listener) {
-        return listener.getPriority();
-    }
+	public static Priority getPriority(PacketListener listener) {
+		return listener.getPriority();
+	}
 
-    public static boolean isRegistered(PacketListener listener) {
-        for (Priority priority : PRIORITIES) {
-			if (PacketManager.listeners.get(priority).contains(listener)) {
+	public static boolean isRegistered(PacketListener listener) {
+		for (Priority priority : PRIORITIES)
+			if (PacketManager.listeners.get(priority).contains(listener))
 				return true;
-			}
-		}
-        return false;
-    }
+		return false;
+	}
 
-    protected static void notify(PacketListener listener, Priority oldPriority, Priority newPriority) {
-        if (listener == null || newPriority == null) {
+	protected static void notify(PacketListener listener, Priority oldPriority, Priority newPriority) {
+		if (listener == null || newPriority == null)
 			return;
-		}
-        if (oldPriority != null) {
+		if (oldPriority != null)
 			PacketManager.listeners.get(oldPriority).remove(listener);
-		}
-        PacketManager.listeners.get(newPriority).add(listener);
-    }
+		PacketManager.listeners.get(newPriority).add(listener);
+	}
 
-    public static void unregisterAll() {
-        for (List<PacketListener> l : PacketManager.listeners.values()) {
+	public static void unregisterAll() {
+		for (List<PacketListener> l : PacketManager.listeners.values())
 			l.clear();
-		}
-    }
+	}
 }
